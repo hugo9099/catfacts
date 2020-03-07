ff6c5b37bd2210f10907fc35648434a643ff248f
Hugo Martinez <hugo9099@yahoo.com>
2020-03-07 11:55:04 -0500

    Add API call on PagesCTRLR

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class PagesController extends Controller
 {
@@ -13,9 +14,30 @@ class PagesController extends Controller
         return view('pages.index')->with('title', $title);
     }
 
-    public function getfacts() {
+    /*public function getfacts() {
         $title = 'Cat Facts';
         return view('pages.getfacts')->with('title', $title);
+    }*/
+
+    public function getfacts()
+    {
+        $title = 'Cat Facts';
+
+        // Api Call
+        $client = new Client();
+        $response = $client->request('GET', 'https://catfact.ninja/facts?limit=5&max_length=140');
+        $statusCode = $response->getStatusCode();
+        $body = $response->getBody()->getContents();
+
+        $body = json_decode($body);
+        $facts_data = $body->data;
+
+        $data = array(
+            'title' => $title,
+            'facts' => $facts_data
+        );
+
+        return view('pages.getfacts')->with($data);
     }
 
     public function about() {
--- resources/views/pages/getfacts.blade.php
+++ resources/views/pages/getfacts.blade.php
@@ -3,4 +3,14 @@
 @section('content')
     <h1>{{$title}}</h1>
     <p>Here you have some facts about cats.</p>
+
+    @if(count($facts) > 0)
+        <ul class="list-group">
+            @foreach($facts as $fact)
+                <li class="list-group-item">
+                    {{$fact->fact}}
+                </li>
+            @endforeach
+        </ul>
+    @endif
 @endsection
